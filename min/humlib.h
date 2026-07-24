f946d8669c7dcb11f0f41c7c7b11c4dc73607284
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-07-24 09:37:31 +0200

    Fix broken -k and -s option in metweight

--- min/humlib.h
+++ min/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Fr. 24 Juli 2026 09:15:49 CEST
+// Last Modified: Fr. 24 Juli 2026 09:36:39 CEST
 // Filename:      min/humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/min/humlib.h
 // Syntax:        C++11
